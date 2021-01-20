c81fcca3096df9c007c8706bebe451d0dbc4f9dd
Sergey Kornienko <skornienko29@gmail.com>
2021-01-20 15:00:02 +0300

    add gendiff/cli.py gendiff/scripts/gendiff.py

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,16 +1,11 @@
 """Scripts for power gendiff"""
 
-import argparse
+from gendiff import cli
 
 
 def main():
     """launch gendiff"""
-    parser = argparse.ArgumentParser(description='Generate diff')
-    parser.add_argument('first_file')
-    parser.add_argument('second_file')
-    parser.add_argument('-f', '--format', help='set format of output')
-    args = parser.parse_args()
-    print(args.accumulate(args.integers))
+    cli.run()
 
 
 if __name__ == '__main__':

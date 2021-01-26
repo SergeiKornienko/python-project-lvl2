8a5e3c731d6b4965f50c7e07f43dadcc46e02589
Sergey Kornienko <skornienko29@gmail.com>
2021-01-26 12:55:04 +0300

    fix pytest

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -9,19 +9,29 @@ def get_diff(file1, file2):  # noqa: WPS210
         file2: dict
 
     Returns:
-        Return string with difference.
+        Return dict with difference.
     """
+    changing = {
+        "added": {},
+        "deleted": {},
+        "changed": {},
+        "unchanged": {}
+    }
     keys = sorted(file1.keys() | file2.keys())
 
-    def inner(key):  # noqa: WPS430
+    for key in keys:
         if key not in file2.keys():
-            return '  - {a}: {b}'.format(a=key, b=file1[key])
+            changing["deleted"].update({key: file1[key]})
         elif key not in file1.keys():
-            return '  + {a}: {b}'.format(a=key, b=file2[key])
+            changing["added"].update({key: file2[key]})
         elif file1.get(key) == file2.get(key):
-            return '    {a}: {b}'.format(a=key, b=file2[key])
-        return '  - {a}: {b}\n  + {a}: {c}'.format(
-            a=key, b=file1[key], c=file2[key],
-        )
-    diff = '\n'.join(list(map(inner, keys)))
-    return '\n'.join(['{', diff, '}']).lower()
+            changing["unchanged"].update({key: file1[key]})
+        elif file1.get(key) != file2.get(key) \
+                and isinstance(file1.get(key), dict)\
+                and isinstance(file2.get(key), dict):
+            changing["changed"].update({key: get_diff(file1[key], file2[key])})
+        else:
+            changing["deleted"].update({key: file1[key]})
+            changing["added"].update({key: file2[key]})
+    return changing
+
--- tests/test_generate_diff_yaml.py
+++ tests/test_generate_diff_yaml.py
@@ -1,72 +1,74 @@
 """Module of tests generate_diff."""
 import pathlib
 
+import json
+
 from gendiff.generate_diff import get_diff
 from gendiff.opening_files import get_content
 
-PATH_FILE_DIFF = 'tests/fixtures/diff_file1_file2.txt'
+PATH_FILE_DIFF = 'tests/fixtures/diff_file1_file2.json'
 
 
 def test_gen_diff_yaml_two_files():
     """Test of function generate_diff with two files."""
-    with open('tests/fixtures/expected_file1_file2.txt') as infile:
-        expected = infile.read()
+    with open('tests/fixtures/expected_file1_file2.json') as infile:
+        expected = json.load(infile)
     with open(PATH_FILE_DIFF, 'w') as diff:
-        diff.write(get_diff(*get_content(
+        json.dump(get_diff(*get_content(
             pathlib.Path('tests/fixtures/file1.yml'),
             pathlib.Path('tests/fixtures/file2.yml'),
-        )))
+        )), diff)
     with open(PATH_FILE_DIFF) as diff1:
-        assert diff1.read() == expected
+        assert json.load(diff1) == expected
 
 
 def test_gen_diff_yaml_empty_files():
     """Test of function generate_diff with two empty files."""
-    with open('tests/fixtures/expected_empty.txt') as infile:
-        expected = infile.read()
+    with open('tests/fixtures/expected_empty.json') as infile:
+        expected = json.load(infile)
     with open(PATH_FILE_DIFF, 'w') as diff:
-        diff.write(get_diff(*get_content(
+        json.dump(get_diff(*get_content(
             pathlib.Path('tests/fixtures/file_empty.yml'),
             pathlib.Path('tests/fixtures/file_empty2.yml'),
-        )))
+        )), diff)
     with open(PATH_FILE_DIFF) as diff1:
-        assert diff1.read() == expected
+        assert json.load(diff1) == expected
 
 
 def test_gen_diff_yaml_file_empty():
     """Test of function generate_diff with first file and second empty file."""
-    with open('tests/fixtures/expected_file1_empty.txt') as infile:
-        expected = infile.read()
+    with open('tests/fixtures/expected_file1_empty.json') as infile:
+        expected = json.load(infile)
     with open(PATH_FILE_DIFF, 'w') as diff:
-        diff.write(get_diff(*get_content(
+        json.dump(get_diff(*get_content(
             pathlib.Path('tests/fixtures/file1.yml'),
             pathlib.Path('tests/fixtures/file_empty.yml'),
-        )))
+        )), diff)
     with open(PATH_FILE_DIFF) as diff1:
-        assert diff1.read() == expected
+        assert json.load(diff1) == expected
 
 
 def test_gen_diff_yaml_empty_file():
     """Test of function generate_diff with first empty file and second file."""
-    with open('tests/fixtures/expected_empty_file1.txt') as infile:
-        expected = infile.read()
+    with open('tests/fixtures/expected_empty_file1.json') as infile:
+        expected = json.load(infile)
     with open(PATH_FILE_DIFF, 'w') as diff:
-        diff.write(get_diff(*get_content(
+        json.dump(get_diff(*get_content(
             pathlib.Path('tests/fixtures/file_empty.yml'),
             pathlib.Path('tests/fixtures/file1.yml'),
-        )))
+        )), diff)
     with open(PATH_FILE_DIFF) as diff1:
-        assert diff1.read() == expected
+        assert json.load(diff1) == expected
 
 
 def test_gen_diff_yaml_nested():
     """Test of function generate_diff with nested files."""
-    with open('tests/fixtures/expected_nested_files.txt') as infile:
-        expected = infile.read()
+    with open('tests/fixtures/expected_nested_files.json') as infile:
+        expected = json.load(infile)
     with open(PATH_FILE_DIFF, 'w') as diff:
-        diff.write(get_diff(*get_content(
+        json.dump(get_diff(*get_content(
             pathlib.Path('tests/fixtures/file_nested1.yml'),
             pathlib.Path('tests/fixtures/file_nested2.yml'),
-        )))
+        )), diff)
     with open(PATH_FILE_DIFF) as diff1:
-        assert diff1.read() == expected
+        assert json.load(diff1) == expected

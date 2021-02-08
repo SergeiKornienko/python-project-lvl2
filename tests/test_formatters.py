0e1ca90419827ec319135bc08e0eb3d86fd78fe5
Sergey Kornienko <skornienko29@gmail.com>
2021-02-08 18:45:19 +0300

    mod tests summoning

---
+++ tests/test_formatters.py
@@ -0,0 +1,77 @@
+"""Module of tests formatters."""
+import pathlib
+
+from gendiff import gendiff
+
+
+def test_generate_diff_stylish_two_simple_files():
+    """Test formatters stylish."""
+    with open('tests/fixtures/expected_file1_file2_stylish.txt') as infile:
+        expected = infile.read()
+    diff = gendiff.generate_diff(
+        pathlib.Path('tests/fixtures/file1.json'),
+        pathlib.Path('tests/fixtures/file2.json'),
+    )
+    assert diff == expected
+
+
+def test_generate_diff_stylish_two_nested_files():
+    """Test formatters stylish."""
+    with open('tests/fixtures/expected_nested_files_stylish.txt') as infile:
+        expected = infile.read()
+    diff = gendiff.generate_diff(
+        pathlib.Path('tests/fixtures/file_nested1.json'),
+        pathlib.Path('tests/fixtures/file_nested2.json'),
+    )
+    assert diff == expected
+
+
+
+
+def test_generate_diff_plain_two_simple_files():
+    """Test formatter plain."""
+    with open('tests/fixtures/expected_file1_file2_plain.txt') as infile:
+        expected = infile.read()
+    diff = gendiff.generate_diff(
+        pathlib.Path('tests/fixtures/file1.json'),
+        pathlib.Path('tests/fixtures/file2.json'),
+        formatter='plain',
+    )
+    assert diff == expected
+
+
+def test_generate_diff_plain_two_nested_files():
+    """Test formatter plain."""
+    with open('tests/fixtures/expected_nested_files_plain.txt') as infile:
+        expected = infile.read()
+    diff = gendiff.generate_diff(
+        pathlib.Path('tests/fixtures/file_nested1.json'),
+        pathlib.Path('tests/fixtures/file_nested2.json'),
+        formatter='plain',
+    )
+    assert diff == expected
+
+
+def test_generate_diff_json_two_simple_files():
+    """Test formatter json."""
+    with open('tests/fixtures/expected_file1_file2_json.json') as infile:
+        expected = infile.read()
+    diff = gendiff.generate_diff(
+        pathlib.Path('tests/fixtures/file1.json'),
+        pathlib.Path('tests/fixtures/file2.json'),
+        formatter='json',
+    )
+    assert diff == expected
+
+
+def test_generate_diff_json_two_nested_files():
+    """Test formatter json."""
+    with open('tests/fixtures/expected_nested_files_json.json') as infile:
+        expected = infile.read()
+    diff = gendiff.generate_diff(
+        pathlib.Path('tests/fixtures/file_nested1.yml'),
+        pathlib.Path('tests/fixtures/file_nested2.yml'),
+        formatter='json',
+    )
+    assert diff == expected
+
--- tests/test_get_diff.py
+++ tests/test_get_diff.py
@@ -86,3 +86,84 @@ def test_get_diff_json_nested():
         )
     with open(PATH_FILE_DIFF) as diff1:
         assert json.load(diff1) == expected
+
+
+
+def test_get_diff_yaml_two_files():
+    """Test of function get_diff with two files."""
+    with open('tests/fixtures/expected_file1_file2.json') as infile:
+        expected = json.load(infile)
+    with open(PATH_FILE_DIFF, 'w') as diff:
+        json.dump(
+            get_diff(
+                parse(*open_file(pathlib.Path('tests/fixtures/file1.yml'))),
+                parse(*open_file(pathlib.Path('tests/fixtures/file2.yml'))),
+            ),
+            diff,
+        )
+    with open(PATH_FILE_DIFF) as diff1:
+        assert json.load(diff1) == expected
+
+
+def test_get_diff_yaml_empty_files():
+    """Test of function get_diff with two empty files."""
+    with open('tests/fixtures/expected_empty.json') as infile:
+        expected = json.load(infile)
+    with open(PATH_FILE_DIFF, 'w') as diff:
+        json.dump(
+            get_diff(
+                parse(*open_file(pathlib.Path('tests/fixtures/file_empty.yml'))),
+                parse(*open_file(pathlib.Path('tests/fixtures/file_empty2.yml'))),
+            ),
+            diff,
+        )
+    with open(PATH_FILE_DIFF) as diff1:
+        assert json.load(diff1) == expected
+
+
+def test_get_diff_yaml_file_empty():
+    """Test of function get_diff with first file and second empty file."""
+    with open('tests/fixtures/expected_file1_empty.json') as infile:
+        expected = json.load(infile)
+    with open(PATH_FILE_DIFF, 'w') as diff:
+        json.dump(
+            get_diff(
+                parse(*open_file(pathlib.Path('tests/fixtures/file1.yml'))),
+                parse(*open_file(pathlib.Path('tests/fixtures/file_empty.yml'))),
+            ),
+            diff,
+        )
+    with open(PATH_FILE_DIFF) as diff1:
+        assert json.load(diff1) == expected
+
+
+def test_get_diff_yaml_empty_file():
+    """Test of function get_diff with first empty file and second file."""
+    with open('tests/fixtures/expected_empty_file1.json') as infile:
+        expected = json.load(infile)
+    with open(PATH_FILE_DIFF, 'w') as diff:
+        json.dump(
+            get_diff(
+                parse(*open_file(pathlib.Path('tests/fixtures/file_empty.yml'))),
+                parse(*open_file(pathlib.Path('tests/fixtures/file1.yml'))),
+            ),
+            diff,
+        )
+    with open(PATH_FILE_DIFF) as diff1:
+        assert json.load(diff1) == expected
+
+
+def test_get_diff_yaml_nested():
+    """Test of function get_diff with nested files."""
+    with open('tests/fixtures/expected_nested_files.json') as infile:
+        expected = json.load(infile)
+    with open(PATH_FILE_DIFF, 'w') as diff:
+        json.dump(
+            get_diff(
+                parse(*open_file(pathlib.Path('tests/fixtures/file_nested1.yml'))),
+                parse(*open_file(pathlib.Path('tests/fixtures/file_nested2.yml'))),
+            ),
+            diff,
+        )
+    with open(PATH_FILE_DIFF) as diff1:
+        assert json.load(diff1) == expected
